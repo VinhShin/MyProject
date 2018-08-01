f09b17f3d8881ed8634a29e3153be80843be80b1
vongvinhshin@gmail.com <mylife6172>
2018-08-02 01:02:34 +0700

    + add permission request

--- AutoCall/app/src/main/java/com/example/yongquan/autocall/MainActivity.java
+++ AutoCall/app/src/main/java/com/example/yongquan/autocall/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.yongquan.autocall;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,15 +18,24 @@ import com.example.yongquan.autocall.Global.Global_Variable;
 import com.example.yongquan.autocall.Model.Contact;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button buttonStart, buttonAddPhone,buttonSetting;
 
 
     public static String STATE_PHONE = "";
+    public final int MULTIPLE_PERMISSIONS = 10;
 
     private SharedPreferences sharedPreferences;
-
+    public static String[] permissions = new String[]{
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private boolean permision_ok = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +64,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        permision_ok = true;
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permision_ok = true;
+                } else {
+                    String permissions1 = "";
+                    for (String per : MainActivity.permissions) {
+                        permissions1 += "\n" + per;
+                    }
+                    // permissions list of don't granted permission
+                }
+                return;
+            }
+        }
+    }
+
     private void init(){
 //        final PhoneCallListener phoneListener = new PhoneCallListener();
 //        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
@@ -77,27 +127,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View src) {
         switch (src.getId()) {
             case R.id.buttonStart:
-                if(!Global_Variable.SERVICE_IS_START) {
-                    if(MyService.listContact != null && MyService.listContact.size()>0) {
-                        Global_Variable.SERVICE_IS_START = true;
+                checkPermissions();
+                if(permision_ok) {
+                    if (!Global_Variable.SERVICE_IS_START) {
+                        if (MyService.listContact != null && MyService.listContact.size() > 0) {
+                            Global_Variable.SERVICE_IS_START = true;
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putBoolean(Global_Variable.SERVICE_IS_START_STR, Global_Variable.SERVICE_IS_START);
+                            editor.apply();
+                            startService(new Intent(this, MyService.class));
+                            buttonStart.setText("Tắt Chương Trình");
+                            Toast.makeText(this, "Chạy Chương Trình", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Không có số gọi", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Global_Variable.SERVICE_IS_START = false;
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putBoolean(Global_Variable.SERVICE_IS_START_STR, Global_Variable.SERVICE_IS_START);
                         editor.apply();
-                        startService(new Intent(this, MyService.class));
-                        buttonStart.setText("Tắt Chương Trình");
-                        Toast.makeText(this, "Chạy Chương Trình", Toast.LENGTH_LONG).show();
+                        stopService(new Intent(this, MyService.class));
+                        buttonStart.setText("Chạy Chương Trình");
                     }
-                    else {
-                        Toast.makeText(this, "Không có số gọi", Toast.LENGTH_LONG).show();
-                    }
-                }
-                else {
-                    Global_Variable.SERVICE_IS_START = false;
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean(Global_Variable.SERVICE_IS_START_STR, Global_Variable.SERVICE_IS_START);
-                    editor.apply();
-                    stopService(new Intent(this, MyService.class));
-                    buttonStart.setText("Chạy Chương Trình");
                 }
                 break;
             case R.id.buttonAddPhone:
